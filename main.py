81f1353b7012a31373d5216646a86fa22d9295a1
Mcveigth <mcveigth@protonmail.com>
2022-06-02 21:30:33 -0900

    check function implemented in the code

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 if __name__ == '__main__':
     import game
+
     print("Welcome!\nto this trivia game!")
-    numberOfQuestions = 2
+    numberOfQuestions = 10
     playerScore = 0
     trivia = game.Game(numberOfQuestions)
     triviaApi = game.requests.get("https://opentdb.com/api.php", params=trivia.set_parameters())
@@ -12,8 +13,9 @@ if __name__ == '__main__':
         trivia.questions = num
         print(num.question)
         trivia.display()
-        print(f"debug purposes: {num.answer}")
+        print(f"debug purposes: {num.answer}")  # debug purposes
         answer = input("answer: ")
+        # check if the answer is correct
         if num.check(answer) is True:
             playerScore += 1
     print(f'you got {playerScore} out of {numberOfQuestions}')
